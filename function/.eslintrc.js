1ee22f3b1cebe80c1362defb21b7324ffda9d5c8
Fuad Aliah <eye.design17@yahoo.com>
2025-03-13 04:56:02 +0300

    Update functions package.json for Next.js SSR

---
+++ function/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  extends: ["eslint:recommended", "google"],
+  rules: {
+    "no-restricted-globals": ["error", "name", "length"],
+    "prefer-arrow-callback": "error",
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
+    "space-before-blocks": ["on"], // Add this line to ignore space before {
+  },
+  overrides: [
+    {
+      files: ["**/*.spec.*"],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
+  globals: {},
+};
